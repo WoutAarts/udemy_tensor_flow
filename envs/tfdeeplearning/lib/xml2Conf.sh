977b276e9b36af5d9e47119664985b7e24193b05
Wout Aarts <wout@cytora.com>
2018-03-22 09:20:36 +0000

    First commit

---
+++ envs/tfdeeplearning/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/wout/anaconda3/envs/tfdeeplearning/lib"
+XML2_LIBS="-lxml2 -L/home/wout/anaconda3/envs/tfdeeplearning/lib -lz     -L/home/wout/anaconda3/envs/tfdeeplearning/lib -liconv  -lm "
+XML2_INCLUDEDIR="-I/home/wout/anaconda3/envs/tfdeeplearning/include/libxml2 -I/home/wout/anaconda3/envs/tfdeeplearning/include"
+MODULE_VERSION="xml2-2.9.4"
+
